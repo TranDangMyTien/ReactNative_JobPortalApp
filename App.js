57f05e1f20d1ca8943b205966a3a93f5dd120429
TranDangMyTien <106966807+TranDangMyTien@users.noreply.github.com>
2024-07-16 17:56:32 +0700

    Change the Splash section

--- App.js
+++ App.js
@@ -1,50 +1,52 @@
-import React, { useState } from 'react';
-import 'react-native-gesture-handler';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { MyDispatchContext, MyUserContext } from './configs/Contexts';
-import { useContext, useReducer, useEffect } from 'react';
-import MyUserReducer from './configs/Reducers';
-import { authApi, endpoints,  } from './configs/APIs';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import HomeScreen from './components/Home/HomeScreen';
-import ProfileApplicant from './components/User/Applicant/ProfileApplicant';
-import ProfileEmployer from './components/User/Employer/ProfileEmployer';
-import ProfileAdmin from './components/User/Admin/ProfileAdmin';
-import Login from './components/User/Login';
-import Register from './components/User/Register/Register';
-import RegisterRole from './components/User/Register/RegisterRole';
-import RegisterApplicant from './components/User/Register/RegisterApplicant';
-import RegisterEmployer from './components/User/Register/RegisterEmployer';
-import PostDetail from './components/RecruitmentsPost/PostDetail';
-import NewPost from './components/RecruitmentsPost/NewPost';
-import ApplyJob from './components/RecruitmentsPost/ApplyJob';
-import FavoriteJobs from './components/RecruitmentsPost/FavoriteJobs';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Profile from './components/User/Normal User/Profile';
-import ForgotPassword from './components/User/ForgotPassword';
-import UpdateUser from './components/User/UpdateUser';
-import AllJobs from './components/RecruitmentsPost/AllJobs';
-import PopularJobs from './components/RecruitmentsPost/PopularJobs';
-import { Provider as PaperProvider } from 'react-native-paper'; 
-import UpdateApplicant from './components/User/Applicant/UpdateApplicant';
-import SuitableJob from './components/User/Applicant/SuitableJob';
-import UpdateEmployer from './components/User/Employer/UpdateEmployer';
-import CreateRecruitment from './components/User/Employer/CreateRecruitment';
-import ListJobPost from './components/User/Employer/ListJobPost';
-import ListApply from './components/User/Applicant/ListApply';
-import FindApplicant from './components/User/Employer/FindApplicant';
-import Splash from './components/SplashScreen/Splash';
+import "react-native-gesture-handler";
+import { StatusBar } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { MyDispatchContext, MyUserContext } from "./configs/Contexts";
+import { useContext, useReducer, useEffect, useState } from "react";
+import MyUserReducer from "./configs/Reducers";
+import { authApi, endpoints } from "./configs/APIs";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import HomeScreen from "./components/Home/HomeScreen";
+import ProfileApplicant from "./components/User/Applicant/ProfileApplicant";
+import ProfileEmployer from "./components/User/Employer/ProfileEmployer";
+import ProfileAdmin from "./components/User/Admin/ProfileAdmin";
+import Login from "./components/User/Login";
+import Register from "./components/User/Register/Register";
+import RegisterRole from "./components/User/Register/RegisterRole";
+import RegisterApplicant from "./components/User/Register/RegisterApplicant";
+import RegisterEmployer from "./components/User/Register/RegisterEmployer";
+import PostDetail from "./components/RecruitmentsPost/PostDetail";
+import NewPost from "./components/RecruitmentsPost/NewPost";
+import ApplyJob from "./components/RecruitmentsPost/ApplyJob";
+import FavoriteJobs from "./components/RecruitmentsPost/FavoriteJobs";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Profile from "./components/User/Normal User/Profile";
+import ForgotPassword from "./components/User/ForgotPassword";
+import UpdateUser from "./components/User/UpdateUser";
+import AllJobs from "./components/RecruitmentsPost/AllJobs";
+import PopularJobs from "./components/RecruitmentsPost/PopularJobs";
+import { Provider as PaperProvider } from "react-native-paper";
+import UpdateApplicant from "./components/User/Applicant/UpdateApplicant";
+import SuitableJob from "./components/User/Applicant/SuitableJob";
+import UpdateEmployer from "./components/User/Employer/UpdateEmployer";
+import CreateRecruitment from "./components/User/Employer/CreateRecruitment";
+import ListJobPost from "./components/User/Employer/ListJobPost";
+import ListApply from "./components/User/Applicant/ListApply";
+import FindApplicant from "./components/User/Employer/FindApplicant";
+import Splash from "./components/SplashScreen/Splash";
 // Cài đặt stack
 const Stack = createStackNavigator();
 
-const MyHome = () => { 
+const MyHome = () => {
   return (
     <PaperProvider>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="JobDetail" component={PostDetail} />
         <Stack.Screen name="NewPost" component={NewPost} />
@@ -52,26 +54,23 @@ const MyHome = () => {
         <Stack.Screen name="FavoriteJobs" component={FavoriteJobs} />
         <Stack.Screen name="AllJobs" component={AllJobs} />
         <Stack.Screen name="PopularJobs" component={PopularJobs} />
-        
       </Stack.Navigator>
     </PaperProvider>
   );
-}
+};
 
 // Màn hình bên đăng ký
 const MyRegister = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-    
-    <Stack.Screen name="Register" component={Register} />
-    <Stack.Screen name="RegisterRole" component={RegisterRole} />
-    <Stack.Screen name="RegisterApplicant" component={RegisterApplicant} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="RegisterRole" component={RegisterRole} />
+      <Stack.Screen name="RegisterApplicant" component={RegisterApplicant} />
 
-    <Stack.Screen name="RegisterEmployer" component={RegisterEmployer} />
-    
+      <Stack.Screen name="RegisterEmployer" component={RegisterEmployer} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
 const MyLogin = () => {
   return (
@@ -79,8 +78,8 @@ const MyLogin = () => {
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 const MyProfileApplicant = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
@@ -89,10 +88,9 @@ const MyProfileApplicant = () => {
       <Stack.Screen name="UpdateApplicant" component={UpdateApplicant} />
       <Stack.Screen name="SuitableJob" component={SuitableJob} />
       <Stack.Screen name="ListApply" component={ListApply} />
-
     </Stack.Navigator>
-  )
-}
+  );
+};
 
 const MyProfileEmployer = () => {
   return (
@@ -104,8 +102,8 @@ const MyProfileEmployer = () => {
       <Stack.Screen name="ListJobPost" component={ListJobPost} />
       <Stack.Screen name="FindApplicant" component={FindApplicant} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
 const Tab = createBottomTabNavigator();
 const MyTab = () => {
@@ -116,35 +114,65 @@ const MyTab = () => {
         tabBarIcon: ({ color, size }) => {
           let iconName;
 
-          if (route.name === 'MyHome') {
-            iconName = 'home';
+          if (route.name === "MyHome") {
+            iconName = "home";
             return <FontAwesome name={iconName} size={size} color={color} />;
-          } else if (route.name === 'MyRegister') {
-            iconName = 'account-plus';
-            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
-          } else if (route.name === 'MyLogin') {
-            iconName = 'login';
-            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
-          } else if (route.name === 'MyProfileApplicant') {
-            iconName = 'account';
-            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
-          } else if (route.name === 'MyProfileEmployer') {
-            iconName = 'office-building';
-            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
-          } else if (route.name === 'Admin') {
-            iconName = 'shield-account';
-            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+          } else if (route.name === "MyRegister") {
+            iconName = "account-plus";
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          } else if (route.name === "MyLogin") {
+            iconName = "login";
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          } else if (route.name === "MyProfileApplicant") {
+            iconName = "account";
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          } else if (route.name === "MyProfileEmployer") {
+            iconName = "office-building";
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          } else if (route.name === "Admin") {
+            iconName = "shield-account";
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
           }
 
           // Không tìm thấy iconName phù hợp, trả về null
           return null;
         },
-        tabBarActiveTintColor: 'black',
-        tabBarInactiveTintColor: 'gray',
+        tabBarActiveTintColor: "black",
+        tabBarInactiveTintColor: "gray",
         tabBarStyle: {
-          backgroundColor: 'white',
+          backgroundColor: "white",
           borderTopWidth: 1,
-          borderTopColor: '#ddd',
+          borderTopColor: "#ddd",
           height: 60,
           paddingBottom: 5,
           paddingTop: 5,
@@ -154,46 +182,74 @@ const MyTab = () => {
         },
       })}
     >
-      <Tab.Screen name="MyHome" component={MyHome} options={{ title: 'Trang chủ' }} />
+      <Tab.Screen
+        name="MyHome"
+        component={MyHome}
+        options={{ title: "Trang chủ" }}
+      />
 
       {user === null ? (
         <>
-            <Tab.Screen name="MyRegister" component={MyRegister} options={{ title: 'Đăng ký' }} />
-            <Tab.Screen name="MyLogin" component={MyLogin} options={{ title: 'Đăng nhập' }} />
+          <Tab.Screen
+            name="MyRegister"
+            component={MyRegister}
+            options={{ title: "Đăng ký" }}
+          />
+          <Tab.Screen
+            name="MyLogin"
+            component={MyLogin}
+            options={{ title: "Đăng nhập" }}
+          />
         </>
       ) : (
-      <>
+        <>
           {user.applicant !== null && (
-              <Tab.Screen name="MyProfileApplicant" component={MyProfileApplicant} options={{ title: 'Ứng viên' }} />
+            <Tab.Screen
+              name="MyProfileApplicant"
+              component={MyProfileApplicant}
+              options={{ title: "Ứng viên" }}
+            />
           )}
           {user.employer !== null && (
-              <Tab.Screen name="MyProfileEmployer" component={MyProfileEmployer} options={{ title: 'Nhà tuyển dụng' }} />
+            <Tab.Screen
+              name="MyProfileEmployer"
+              component={MyProfileEmployer}
+              options={{ title: "Nhà tuyển dụng" }}
+            />
           )}
           {(user.is_staff || user.is_superuser) && (
-              <Tab.Screen name="Admin" component={ProfileAdmin} options={{ title: 'Quản trị viên' }} />
+            <Tab.Screen
+              name="Admin"
+              component={ProfileAdmin}
+              options={{ title: "Quản trị viên" }}
+            />
           )}
-          {user.applicant === null && user.employer === null && !user.is_staff && !user.is_superuser && (
-              <Tab.Screen name="Profile" component={Profile} options={{ title: 'Profile' }} />
-          )}
-      </>
-    )}
-
+          {user.applicant === null &&
+            user.employer === null &&
+            !user.is_staff &&
+            !user.is_superuser && (
+              <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{ title: "Profile" }}
+              />
+            )}
+        </>
+      )}
     </Tab.Navigator>
   );
-}
-
-//Phần Splash 
-const [isShowSplash, serIsShowSplash] = useState(true);
-useEffect(
-  () => {
-    const timeout = setTimeout(() => {
-      serIsShowSplash(false)
-    },1500);
-    return () => clearTimeout(timeout);
-  }, []);
+};
 
 export default function App() {
   const [user, dispatch] = useReducer(MyUserReducer, null);
+  //Phần Splash
+  const [isShowSplash, serIsShowSplash] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      serIsShowSplash(false);
+    }, 50000);
+    return () => clearTimeout(timeout);
+  }, []);
 
   const alreadyLogin = async (retryCount = 0) => {
     try {
@@ -213,10 +269,7 @@ export default function App() {
         let errorStatus;
         if (error.response) {
           errorStatus = error.response.status;
-          if (
-            (errorStatus !== 401 && errorStatus !== 403) ||
-            retryCount > 3
-          ) {
+          if ((errorStatus !== 401 && errorStatus !== 403) || retryCount > 3) {
             dispatch({ type: "logout" });
             return;
           }
@@ -236,14 +289,24 @@ export default function App() {
   }, []);
 
   return (
-    
-    isShowSplash ? <Splash/> : 
-    <NavigationContainer>
-      <MyUserContext.Provider value={user}>
-        <MyDispatchContext.Provider value={dispatch}>
-          <MyTab />
-        </MyDispatchContext.Provider>
-      </MyUserContext.Provider>
-    </NavigationContainer>
+    // Status bar (thanh có số tin, wifi,...)
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {isShowSplash ? (
+        <Splash/>
+      ) : (
+        <NavigationContainer>
+          <MyUserContext.Provider value={user}>
+            <MyDispatchContext.Provider value={dispatch}>
+              <MyTab />
+            </MyDispatchContext.Provider>
+          </MyUserContext.Provider>
+        </NavigationContainer>
+      )}
+    </>
   );
 }
--- components/SplashScreen/Splash.js
+++ components/SplashScreen/Splash.js
@@ -1,10 +1,60 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-const Splash = () => {
+import {
+    ActivityIndicator,
+    ImageBackground,
+    Image,
+    View,
+    StyleSheet,
+  } from "react-native";
+  import React, { useState } from "react";
+  import { appInfo } from "../constants/appInfos";
+  import SpaceComponent from './SpaceComponent'; // Import default
+  
+  const Splash = () => {
+    const [isLoading, setIsLoading] = useState(true);
+  
     return (
-        <View>
-            <Text>Splash</Text>
-        </View>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../../assets/Images/BG_JobPortalApp.png")}
+          style={styles.backgroundImage}
+          imageStyle={{ flex: 1 }}
+          onLoadEnd={() => setIsLoading(false)}
+        >
+           
+            <Image
+              source={require("../../assets/Images/Logo_JobPortalApp_Green.png")}
+              style={styles.logo}
+            />
+        
+          <SpaceComponent height={16} />
+          <ActivityIndicator style={styles.indicator} />
+        </ImageBackground>
+      </View>
     );
-};
-export default Splash;
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    backgroundImage: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+      height: "100%",
+    },
+    logo: {
+      width: appInfo.sizes.WIDTH * 0.7,
+      resizeMode: "contain",
+    },
+    indicator: {
+      color: '#807A7A',
+      size: 22,
+    },
+  });
+  
+  export default Splash;
+  
